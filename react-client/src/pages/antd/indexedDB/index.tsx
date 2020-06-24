8add99d9d4a58843b9297d1b56b7f9c2d46ceeb3
chenqiang03 <chenqiang03@sunlands.com>
2020-06-24 17:37:51 +0800

    feat: indexedDB的写入

--- react-client/src/pages/antd/indexedDB/index.tsx
+++ react-client/src/pages/antd/indexedDB/index.tsx
@@ -2,12 +2,12 @@
  * @description indexedDB
  * @author cq
  * @Date 2020-06-24 11:03:24
- * @LastEditTime 2020-06-24 11:03:35
+ * @LastEditTime 2020-06-24 17:28:55
  * @LastEditors cq
  */
 
 
-import React, { FunctionComponent } from 'react'
+import React, { FunctionComponent, useEffect } from 'react'
 
 type IndexedDBDemoProps = {
 
@@ -15,6 +15,72 @@ type IndexedDBDemoProps = {
 
 
 const IndexedDBDemo: FunctionComponent<IndexedDBDemoProps> = () => {
+  // 这个方法接受两个参数，第一个参数是字符串，表示数据库的名字。如果指定的数据库不存在，就会新建数据库。
+  //第二个参数是整数，表示数据库的版本。如果省略，打开已有数据库时，默认为当前版本；新建数据库时，默认为1。
+  useEffect(() => {
+    var request = window.indexedDB.open("testData");
+    var db: any;
+
+    // 数据库新建成功以后，新增一张叫做person的表格，主键是id。
+
+    // 更好的写法是先判断一下，这张表格是否存在，如果不存在再新建。
+    // request.onupgradeneeded = function (event: any) {
+    //   console.log(111);
+    //   db = event.target.result;
+    //   var objectStore;
+    //   if (!db.objectStoreNames.contains('person')) {
+    //     objectStore = db.createObjectStore('person', { keyPath: 'id' });
+    //   }
+    // }
+
+    // 如果数据记录里面没有合适作为主键的属性，那么可以让 IndexedDB 自动生成主键。
+    // 指定主键为一个递增的整数。
+    // var objectStore = db.createObjectStore(
+    // 'person',
+    // { autoIncrement: true }
+    //);
+
+    // 新建对象仓库以后，下一步可以新建索引
+    request.onupgradeneeded = function (event: any) {
+      db = event.target.result;
+      var objectStore;
+      console.log(db.objectStoreNames.contains('person'), "进不来吗");
+      if (!db.objectStoreNames.contains('person')) {
+        console.log(1111);
+        objectStore = db.createObjectStore('person', { keyPath: 'id' });
+        // IDBObject.createIndex()的三个参数
+        //分别为索引名称、索引所在的属性、配置对象（说明该属性是否包含重复的值）。
+        objectStore.createIndex('name', 'name', { unique: false });
+        objectStore.createIndex('email', 'email', { unique: true });
+        add(event);
+      }
+    }
+
+    function add(event: any) {
+      console.log(event.target);
+      var request = event.target.transaction
+        .objectStore('person',"name")
+        .add({ id: 1, name: '张三', age: 24, email: 'zhangsan@example.com' });
+      request.onsuccess = function (event: any) {
+        console.log('数据写入成功');
+      };
+
+      request.onerror = function (event: any) {
+        console.log('数据写入失败');
+      }
+    }
+    request.onerror = function (event) {
+      console.log('数据库打开报错');
+    };
+    request.onsuccess = function (event) {
+      db = request.result;
+      console.log('数据库打开成功', request);
+
+
+    };
+
+
+  }, [])
 
   return (
     <div>
