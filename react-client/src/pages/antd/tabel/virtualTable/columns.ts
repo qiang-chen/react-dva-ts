b3ec2aefca2e75fa96ba41321a5697f22ec643f8
玉麟 <chenqiang03@sunlands.com>
2020-07-08 14:27:43 +0800

    feat: 虚拟tabel滚动

---
+++ react-client/src/pages/antd/tabel/virtualTable/columns.ts
@@ -0,0 +1,38 @@
+/**
+ * @description 
+ * @author cq
+ * @Date 2020-07-08 14:05:56
+ * @LastEditTime 2020-07-08 14:06:28
+ * @LastEditors cq
+ */
+const columns = [
+  {
+    title: 'A',
+    dataIndex: 'key',
+    width: 150,
+  },
+  {
+    title: 'B',
+    dataIndex: 'key',
+  },
+  {
+    title: 'C',
+    dataIndex: 'key',
+  },
+  {
+    title: 'D',
+    dataIndex: 'key',
+  },
+  {
+    title: 'E',
+    dataIndex: 'key',
+    width: 200,
+  },
+  {
+    title: 'F',
+    dataIndex: 'key',
+    width: 100,
+  },
+];
+
+export default columns;
--- react-client/src/pages/antd/tabel/virtualTable/index.tsx
+++ react-client/src/pages/antd/tabel/virtualTable/index.tsx
@@ -2,33 +2,115 @@
  * @description 虚拟列表
  * @author cq
  * @Date 2020-06-28 15:07:52
- * @LastEditTime 2020-07-08 13:55:09
+ * @LastEditTime 2020-07-08 14:25:11
  * @LastEditors cq
  */
 
 
 
-import React, { FunctionComponent } from 'react'
+import React, { FunctionComponent, useState, useEffect, useRef } from 'react';
 import { Table, Modal } from 'antd';
-// import columns from "./columns"
-// import data from './data';
-// import childData from './childData';
-// import style from "../index.module.less"
+import classNames from 'classnames';
+import { VariableSizeGrid as Grid } from 'react-window';
+import ResizeObserver from 'rc-resize-observer';
+import columns from "./columns"
 
 type VirtualTableProps = {
   isVisable: boolean
   onSubmit: () => void
   onClose: () => void
 }
+const dataSource = Array.from(
+  {
+    length: 100000,
+  },
+  (_, key) => ({
+    key,
+  }),
+);
+
+const scroll = {
+  y: 300,
+  x: '100vw',
+}
 
 
 const VirtualTable: FunctionComponent<VirtualTableProps> = ({ isVisable, onClose, onSubmit }) => {
 
+  // const { scroll, className } = props;
+  const [tableWidth, setTableWidth] = useState(0);
 
-  // const getColumns = () => {
-  //   return columns({})
-  // }
- 
+  const widthColumnCount = columns.filter(({ width }) => !width).length;
+
+  const mergedColumns = columns.map(column => {
+    if (column.width) {
+      return column;
+    }
+
+    return {
+      ...column,
+      width: Math.floor(tableWidth / widthColumnCount),
+    };
+  });
+
+  const gridRef = useRef<any>();
+  const [connectObject] = useState<any>(() => {
+    const obj = {};
+    Object.defineProperty(obj, 'scrollLeft', {
+      get: () => null,
+      set: (scrollLeft: number) => {
+        if (gridRef.current) {
+          gridRef.current.scrollTo({ scrollLeft });
+        }
+      },
+    });
+
+    return obj;
+  });
+  const resetVirtualGrid = () => {
+    gridRef.current.resetAfterIndices({
+      columnIndex: 0,
+      shouldForceUpdate: false,
+    });
+  };
+
+  useEffect(() => resetVirtualGrid, []);
+  useEffect(() => resetVirtualGrid, [tableWidth]);
+
+  const renderVirtualList = (rawData: object[], { scrollbarSize, ref, onScroll }: any) => {
+    ref.current = connectObject;
+
+    return (
+      <Grid
+        ref={gridRef}
+        className="virtual-grid"
+        columnCount={mergedColumns.length}
+        columnWidth={index => {
+          const { width } = mergedColumns[index];
+          return index === mergedColumns.length - 1 ? width - scrollbarSize - 1 : width;
+        }}
+        height={scroll.y}
+        rowCount={rawData.length}
+        rowHeight={() => 54}
+        width={tableWidth}
+        onScroll={({ scrollLeft }) => {
+          onScroll({ scrollLeft });
+        }}
+      >
+        {({ columnIndex, rowIndex, style }) => (
+          <div
+            className={classNames('virtual-table-cell', {
+              'virtual-table-cell-last': columnIndex === mergedColumns.length - 1,
+            })}
+            style={style}
+          >
+            {rawData[rowIndex][mergedColumns[columnIndex].dataIndex]}
+          </div>
+        )}
+      </Grid>
+    );
+  };
+
   return (
     <Modal
       title="tabel展开子项滑动"
@@ -38,17 +120,38 @@ const VirtualTable: FunctionComponent<VirtualTableProps> = ({ isVisable, onClose
       onCancel={onClose}
       onOk={onSubmit}
     >
-      <Table
-        rowKey="id"
-        // expandedRowKeys={["1"]}
-        // columns={getColumns() as any}
-        // dataSource={data}
-        scroll={{ x: 478 }}
-        pagination={false}
-      />
+      <ResizeObserver
+        onResize={({ width }) => {
+          setTableWidth(width);
+        }}
+      >
+        <Table
+          {...props}
+          className={classNames(className, 'virtual-table')}
+          columns={mergedColumns}
+          pagination={false}
+          components={{
+            body: renderVirtualList,
+          }}
+        />
+      </ResizeObserver>
     </Modal>
   )
 }
 
 
 export default React.memo(VirtualTable)
+
+
+
+
+
+
+// ReactDOM.render(
+//   <VirtualTable
+//     columns={columns}
+//     dataSource={data}
+
+//   />,
+//   mountNode,
+// );
